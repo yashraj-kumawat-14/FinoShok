58776256e88f63ab5fa7d64166aa7096eb4dce71
yashraj-kumawat-14 <yashrajkumawat7357@gmail.com>
2024-02-26 20:48:01 +0530

    added functionality in calculator

--- calculator.py
+++ calculator.py
@@ -17,6 +17,7 @@ class Calculator:
         container = Frame(calcWindow, bg="grey", width=200, height=250)
         container.pack(fill=BOTH, expand=1)
         container.pack_propagate(0) #By setting pack_propagate(0), the frame maintains its specified size (width and height), and it does not automatically adjust to fit its child widgets.
+        
 
         displayBox = Frame(container, bg="white")
         displayBox.pack(fill=X)
@@ -40,56 +41,81 @@ class Calculator:
         displayEntry.pack(fill=X, ipady=10)
 
         #number buttons
-        button7 = Button(buttonBox, text="7", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("7", self.result, displayVar))
+        button7 = Button(buttonBox, text="7", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("7", displayVar))
         button7.grid(row=0, column=0, pady=1, padx=1)
 
-        button8 = Button(buttonBox, text="8", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("8", self.result, displayVar))
+        button8 = Button(buttonBox, text="8", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("8", displayVar))
         button8.grid(row=0, column=1, pady=1, padx=1)
 
-        button9 = Button(buttonBox, text="9", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("9", self.result, displayVar))
+        button9 = Button(buttonBox, text="9", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("9", displayVar))
         button9.grid(row=0, column=2, pady=1, padx=1)
 
-        button4 = Button(buttonBox, text="4", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("4", self.result, displayVar))
+        button4 = Button(buttonBox, text="4", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("4", displayVar))
         button4.grid(row=1, column=0, pady=1, padx=1)
 
-        button5 = Button(buttonBox, text="5", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("5", self.result, displayVar))
+        button5 = Button(buttonBox, text="5", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("5", displayVar))
         button5.grid(row=1, column=1, pady=1, padx=1)
 
-        button6 = Button(buttonBox, text="6", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("6", self.result, displayVar))
+        button6 = Button(buttonBox, text="6", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("6", displayVar))
         button6.grid(row=1, column=2, pady=1, padx=1)
 
-        button1 = Button(buttonBox, text="1", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("1", self.result, displayVar))
+        button1 = Button(buttonBox, text="1", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("1", displayVar))
         button1.grid(row=2, column=0, pady=1, padx=1)
 
-        button2 = Button(buttonBox, text="2", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("2", self.result, displayVar))
+        button2 = Button(buttonBox, text="2", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("2", displayVar))
         button2.grid(row=2, column=1, pady=1, padx=1)
 
-        button3 = Button(buttonBox, text="3", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("3", self.result, displayVar))
+        button3 = Button(buttonBox, text="3", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("3", displayVar))
         button3.grid(row=2, column=2, pady=1, padx=1)
 
-        button0 = Button(buttonBox, text="0", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("0", self.result, displayVar))
+        button0 = Button(buttonBox, text="0", width=6, height=2, bg=numBg, fg=numFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("0", displayVar))
         button0.grid(row=3, column=1, pady=1, padx=1)
 
         #operational buttons
-        buttonC = Button(buttonBox, text="C", width=5, height=2, bg="red", fg="black", font="COPPER 13 bold",command=lambda : self.operationAndNum("C", self.result, displayVar))
+        buttonC = Button(buttonBox, text="C", width=5, height=2, bg="red", fg="black", font="COPPER 13 bold",command=lambda : self.operationAndNum("C", displayVar))
         buttonC.grid(row=0, column=3, pady=1, padx=1)
         
-        buttonMinus = Button(buttonBox, text="-", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("-", self.result, displayVar))
+        buttonMinus = Button(buttonBox, text="-", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("-", displayVar))
         buttonMinus.grid(row=1, column=3, pady=1, padx=1)
         
-        buttonMul = Button(buttonBox, text="*", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("*", self.result, displayVar))
+        buttonMul = Button(buttonBox, text="*", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("*", displayVar))
         buttonMul.grid(row=2, column=3, pady=1, padx=1)
         
-        buttondiv = Button(buttonBox, text="/", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("/", self.result, displayVar))
+        buttondiv = Button(buttonBox, text="/", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("/", displayVar))
         buttondiv.grid(row=3, column=2, pady=1, padx=1)
         
         buttonAns = Button(buttonBox, text="=", width=5, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda: self.ans(displayVar))
         buttonAns.grid(row=3, column=3, pady=1, padx=1)
         
-        buttonPlus = Button(buttonBox, text="+", width=6, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("+", self.result, displayVar))
+        buttonPlus = Button(buttonBox, text="+", width=6, height=2, bg=commandBg, fg=commandFg, font="COPPER 13 bold", command=lambda : self.operationAndNum("+", displayVar))
         buttonPlus.grid(row=3, column=0, pady=1, padx=1)
-    
-    def operationAndNum(self, operationName, jj, displayVar):
+
+        def keyBoardHandler(event):
+            if(event.char.isdigit()):
+                self.operationAndNum(event.char, displayVar)
+            elif(event.char == "/"):
+                self.operationAndNum(event.char, displayVar)
+            elif(event.char == "*"):
+                self.operationAndNum(event.char, displayVar)
+            elif(event.char == "+"):
+                self.operationAndNum(event.char, displayVar)
+            elif(event.char == "-"):
+                self.operationAndNum(event.char, displayVar)
+            elif(event.char == "="):
+                self.ans(displayVar)
+            elif(event.keysym == "Return"):
+                self.ans(displayVar)
+            else:
+                pass
+
+         #by default the focus will be given to root window as it is created first so i moved the focus to calculator window in first
+        container.focus_set()
+
+        #now i bind the container frame with Key event so that when i press numbers on keyboard gui can recognize them
+        container.bind("<Key>", keyBoardHandler)
+ 
+    #this function displays the entry of previous result and the operations or numbers sum as a string on the display
+    def operationAndNum(self, operationName, displayVar):
         if operationName=="C":
             self.result=""
             displayVar.set(self.result)
@@ -97,7 +123,7 @@ class Calculator:
             self.result+=operationName
             displayVar.set(self.result)
 
-
+    #this ans function evaluates the result in the string format and update result and show it to the display window
     def ans(self, displayVar):
         ans = eval(self.result)
         self.result= str(ans)
