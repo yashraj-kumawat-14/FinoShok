32311377a4c048bdad8553b08804c7ce8503d82d
yashraj-kumawat-14 <yashrajkumawat7357@gmail.com>
2024-03-14 20:25:18 +0530

    added crud operations in Model class of  model file

--- database/database.py
+++ database/database.py
@@ -1,25 +1,59 @@
 #importing necessary modules and variables
-import mysql.connector
-from config.databaseConfig import HOSTNAME, USER, PASSWORD, DATABASE
+import mysql.connector 
+import sys #importing sys module so that we can add config folder to search path
+
+#adding config folder to search path
+sys.path.append(r"D:\projects\finoshok\finoshok\config")
+
+#importing necessary things present in databaseConfig.py file present in config folder
+from databaseConfig import HOSTNAME, USER, PASSWORD, DATABASE
 
 class Database:
-    def __init__(self, query):
+    def fetchData(self, query):
         #making connection with database
         conn = mysql.connector.connect(host=HOSTNAME, user=USER, password=PASSWORD, database=DATABASE)
 
         #creating cursor to perform queries and get results
         cursor = conn.cursor()
 
+        try:
+             #executing query
+            cursor.execute(query)
+            #fetching data
+            data = cursor.fetchall()
+            #closing connections with server and database
+            cursor.close()
+            conn.close()
+            return data  
+        except mysql.connector.Error as error:
+            print("error in the query", error)
+            #undoing the changes made by wrong query
+            conn.rollback()
+            #returning false as symbol of error in query
+            return False
+    
+    def dataModify(self, query):
+        #making connection with database
+        conn = mysql.connector.connect(host='localhost', user="root", password="1234", database="finoshok11")
+
+        #creating cursor to perform queries and get results
+        cursor = conn.cursor()
+
         #executing query
-        result = cursor.execute(query)
-
-        cursor.close()
-        conn.close()
-
-        #checking if result is not empty
-        if(result):
-            return result
-        else:
-            #if result is empty then return False
+        try:
+            #executing query
+            cursor.execute(query)
+            #commiting the changes
+            conn.commit()
+            # losing connections with server and database
+            cursor.close()
+            conn.close()
+            return True   
+        except mysql.connector.Error as error:
+            print("error in the query", error)
+            #undoing teh changes made during wrong query execution
+            conn.rollback()
+            #returning false means query execution failed
             return False
         
+    
---
+++ model.py
@@ -0,0 +1,128 @@
+# imorting database class to make connection with database and execute quereis and fetch data accordingly
+from database.database import Database
+
+class Model:
+    #should be changed while dealing with another table
+    fillable=["col1", "col2", "col3", "col4", "col5"]
+    tableName= "tableName"
+
+    def rowCount(self):
+        #creating a database object
+        object = Database()
+
+        #getting all data and using len to determine the num of rows
+        numOfRows = len(object.fetchData(f"select * from `{self.tableName}`"))
+
+        #returning the row numbers
+        return numOfRows
+    
+    def readAllData(self):
+        #creating a database object
+        object = Database()
+
+        data = object.fetchData(f"select * from `{self.tableName}`")
+
+        #returning the data
+        return data
+    
+    def readData(self, numRow):
+        #creating a database object
+        object = Database()
+
+        data = object.fetchData(f"select * from `{self.tableName}`")[0:numRow] #getting n number of rows from the list
+
+        #returning the data
+        return data
+    
+    #it takes arbitary keyword arguments parameters in the dictionary and filters them and insert into the table
+    def insertData(self, **kwargs):
+        #it checks if self.fillable is empty or not . if it is empty then it will not procced the suite
+        if(self.fillable):
+            #assingning the kwargs to data variable
+            data = kwargs
+
+            #tempKeys holds the keys present data dictionary. these keys are temporary and might change due to filtration in next lines of code
+            tempKeys = list(data.keys())
+
+            # iterating through tempKeys
+            for key in tempKeys:
+                #if there is a key which isn't in self.fillable then it will remove it from the data
+                if(key not in self.fillable):
+                    data.pop(key)
+
+            #creating a empty list
+            columns=list()
+
+            #again creating tempKeys to get latest keys present in the data after filtration
+            tempKeys = list(data.keys())
+
+            #iterating through tempkeys to manipulate string and do some necessary concatenation before executing or inserting the data
+            for key in tempKeys:
+                columns.append("`"+key+"`")
+            
+            #join function is a string method that concatenates the elements of an iterable such as list into a single string , using a specified separator.
+            columns=" ,".join(columns)
+
+
+            values=list()
+            tempValues = list(data.values())
+
+            for value in tempValues:
+                values.append("'"+value+"'")
+            values=" ,".join(values)
+
+
+            #creating a database object
+            object = Database()
+
+            #returning true means succeessful insertion and false for unsuccessfull  insertion
+            return (object.dataModify(f"INSERT INTO `{self.tableName}` ({columns}) values ({values})"))
+        
+    def deleteRow(self, id):
+        #creating a database object
+        object = Database()
+
+        #returning true means succeessful deletion and false for unsuccessfull  deletion of row
+        return (object.dataModify(f"delete from `{self.tableName}` where id={id}"))
+
+    def updateData(self, id, **kwargs):
+         #it checks if self.fillable is empty or not . if it is empty then it will not procced the suite
+        if(self.fillable):
+            #assingning the kwargs to data variable
+            data = kwargs
+
+            #tempKeys holds the keys present data dictionary. these keys are temporary and might change due to filtration in next lines of code
+            tempKeys = list(data.keys())
+
+            # iterating through tempKeys
+            for key in tempKeys:
+                #if there is a key which isn't in self.fillable then it will remove it from the data
+                if(key not in self.fillable):
+                    data.pop(key)
+
+            #creating a empty list
+            columns=list()
+
+            #again creating tempKeys to get latest keys present in the data after filtration
+            tempKeys = list(data.keys())
+
+            #iterating through tempkeys to manipulate string and do some necessary concatenation before executing or inserting the data
+            for key in tempKeys:
+                columns.append("`"+key+"`")
+
+            values=list()
+            tempValues = list(data.values())
+
+            for value in tempValues:
+                values.append("'"+value+"'")
+
+            #FORMATED STRING
+            formattedString = ""
+            for i in range(len(data)):
+                formattedString+= columns[i]+"="+values[i]
+            
+            #creating a database object
+            object = Database()
+
+            #returning true means succeessful insertion and false for unsuccessfull  insertion
+            return (object.dataModify(f"update `{self.tableName}` SET {formattedString} where `id` = {id}"))
