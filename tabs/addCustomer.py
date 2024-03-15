f73aa4ab331417295c5b5bbdf882df260b6c8e65
yashraj-kumawat-14 <yashrajkumawat7357@gmail.com>
2024-03-15 17:19:16 +0530

    added dynamic width feature to entries

--- tabs/addCustomer.py
+++ tabs/addCustomer.py
@@ -7,6 +7,10 @@ class AddCustomer:
         mainFrameColor="black"
         subFrameColor = "black"
 
+        #heading label of the tab
+        addCustomerLabel = Label(addCustomerWindow, text="Add Customer", font="COPPER 15", fg="yellow", bg="black")
+        addCustomerLabel.pack(side="top", fill="x", ipady=20)
+
         #created mainFrame which will hold everything of AddCustomer page
         mainFrame = Frame(addCustomerWindow, bg=mainFrameColor)
         mainFrame.pack(fill="both", expand=True)
@@ -45,9 +49,6 @@ class AddCustomer:
         photoLabel = Label(subFrame, text="Photo : ", fg=labelFgColor, bg=labelBgColor)
         photoLabel.grid(row=6, column=0)
 
-        docVerifyLabel = Label(subFrame, text="Documents Verified : ", fg=labelFgColor, bg=labelBgColor)
-        docVerifyLabel.grid(row=7, column=0)
-
         #creating entries corresponding to their names labels
         cNameEntry = Entry(subFrame, width=14)
         cNameEntry.grid(row=0, column=1)
@@ -67,16 +68,45 @@ class AddCustomer:
         wAddressEntry = Entry(subFrame, width=14)
         wAddressEntry.grid(row=5, column=1)
 
+        #this function changes widhth of entries according to the no. of characters present in entry
+        #it takes two parameter one is event(default para) and one is entryList(necessary) which contains all entries whose width shouild be chnaged
+        
+        def dynamicWidth(event, entryList):
+            #intitially condition is set to True 
+            condition=True
+
+            #traversing through all entries
+            for entry in entryList:
+                #checking if any one entry has no. of character more than 13
+                if(len(entry.get())>13):
+                    #if more than 13 then all entries width will be configured or set to their length +1
+                    for otherEntry in entryList:
+                        otherEntry.config(width=len(entry.get())+1)
+                    #now condition is set to false
+                    condition = False
+
+            #if condition is true till now that means none of the entry in entryList has length of characters more than 13 so we set the width of all entries to 14
+            
+            if(condition):
+                #traversing through all entries
+                for otherEntry in entryList:
+                        #configuring widht of entries
+                        otherEntry.config(width=14)
+
+        #creating entryList variable containing all entry widgets whom we wnat to enable dynamic width
+        entryList = [cNameEntry, aadharEntry, mobileEntry, fNameEntry, hAddressEntry, wAddressEntry]
+
+        #traversing through all entry widgets present in entryList
+        for entry in entryList:
+            #binding each and everyEntry to Key event and function func is set to dynamicWidth
+            #lambda is used to make anonymous function syntax : lambda arguments : expression
+            entry.bind("<Key>", func= lambda event, entryList=entryList: dynamicWidth(event, entryList))
+
+        def photoUpload():
+            pass
         #this button will be used to upload photos
-        photoUploadButton = Button(subFrame, text="Upload", font="COPPER 8", width=13)
+        photoUploadButton = Button(subFrame, text="Upload", font="COPPER 8", width=13, command=photoUpload)
         photoUploadButton.grid(row=6, column=1)
-
-        #verifyVar stores current state of yesCheck checkbutton
-        verifyVar = IntVar()
-
-        #yescheck button
-        yesCheck = Checkbutton(subFrame, text="Yes", variable=verifyVar, fg="black", bg="white")
-        yesCheck.grid(row=7, column=1)
 
         #this checkAndSaveButton
         checkAndSaveButton = Button(mainFrame, text="Check And Save")
