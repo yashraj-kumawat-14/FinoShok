e451768236d0d4ee1990c4503a6a845c891c88f3
Yashraj Kumawat <147941886+yashraj-kumawat-14@users.noreply.github.com>
2024-11-27 15:47:10 +0530

    Update databaseConfig.py

--- config/databaseConfig.py
+++ config/databaseConfig.py
@@ -1,6 +1,9 @@
 #creating initial configurations relavent to database
 
+# set these according to you
+USER = "root" # username of an user on mysql
+PASSWORD = "1234" # mysql user password
+PORT = 3306 # mysql port
+
 HOSTNAME = "localhost"
-PASSWORD = "1234"
-USER = "root"
 DATABASE = "finoshok"
